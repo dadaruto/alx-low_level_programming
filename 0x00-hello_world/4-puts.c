9d69716c4db3faa36f4c530f169b8b09335a3b41
dadaruto <cheladorcas.ruto@gmail.com>
2023-03-17 06:21:34 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a line with functions
+ * Return: 0 (success)
+ */
+
+int main(void)
+
+{
+	put("Programming is like building a multilingual puzzle, followed by a new line.");
+	return (0);
+}
+
+
