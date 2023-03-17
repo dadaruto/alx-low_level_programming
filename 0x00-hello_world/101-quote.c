e5a046c4f6c4656ef97a172dfaa0851ec01d5174
dadaruto <cheladorcas.ruto@gmail.com>
2023-03-17 10:33:46 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main-This is the main fuction description
+ *
+ *Return: 1 for succesful exit
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
+
