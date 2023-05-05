a43235eab5296a0a51c1deb36e2bb592dcd6a3cb
dadaruto <cheladorcas.ruto@gmail.com>
2023-05-04 17:33:17 -0700

    Bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,7 +2,7 @@
 
 /**
  * print_binary - prints the binary representation of a number
- * @n: number to print in binary
+ * @n: number to be printed in binary
  */
 void print_binary(unsigned long int n)
 {
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
-#ifndef main
-#define main
+#ifndef MAIN_H
+#define MAIN_H
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
